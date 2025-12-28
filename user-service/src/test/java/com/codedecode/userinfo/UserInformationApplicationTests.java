20ca26fbbc8829973abd482f6425c006d4a74e2f
amitk70 <61980933+amitk70@users.noreply.github.com>
2025-12-28 15:09:32 +0530

    Update UserInformationApplicationTests.java

--- user-service/src/test/java/com/codedecode/userinfo/UserInformationApplicationTests.java
+++ user-service/src/test/java/com/codedecode/userinfo/UserInformationApplicationTests.java
@@ -10,4 +10,23 @@ class UserInformationApplicationTests {
 	void contextLoads() {
 	}
 
+	@BeforeMethod
+    public void setUp() {
+        User user = new User();
+    }
+
+    // Test for the 'add' method
+    @Test
+    public void testAdd() {
+        int result = user.add(5, 3);
+        // Assert that the result of 5 + 3 is 8
+        Assert.assertEquals(result, 8, "Incorrect");
+    }
+
+    // Test for the 'subtract' method
+    @Test
+    public void testSubtract() {
+        int result = user.subtract(5, 3);
+        // Assert that the result of 5 - 3 is 2
+        Assert.assertEquals(result, 2, "Incorrect");
 }
