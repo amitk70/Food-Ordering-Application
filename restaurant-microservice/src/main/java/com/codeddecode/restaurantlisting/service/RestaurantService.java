d9c386b1411b5f6c3f1f5c2a8e732971f35cdd80
amitk70 <61980933+amitk70@users.noreply.github.com>
2026-01-20 23:26:54 +0530

    Update RestaurantService.java

--- restaurant-microservice/src/main/java/com/codeddecode/restaurantlisting/service/RestaurantService.java
+++ restaurant-microservice/src/main/java/com/codeddecode/restaurantlisting/service/RestaurantService.java
@@ -13,7 +13,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
- 
+
+
 @Service
 public class RestaurantService {
 
